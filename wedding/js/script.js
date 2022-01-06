daa841fe3122409022f16bbdf50e756cf9b80c20
dnlam <dinhngoclambk@gmail.com>
2022-01-06 16:08:27 +0100

    change photos ok
    changes photos of the website ok

--- wedding/js/script.js
+++ wedding/js/script.js
@@ -470,7 +470,7 @@
         = COUNTDOWN CLOCK
     -------------------------------------------*/
     if ($("#clock").length) {
-        $('#clock').countdown('2021/08/21', function(event) {
+        $('#clock').countdown('2022/02/26', function(event) {
             var $this = $(this).html(event.strftime(''
             + '<div class="box"><div>%D</div> <span>Days</span> </div>'
             + '<div class="box"><div>%H</div> <span>Hours</span> </div>'
